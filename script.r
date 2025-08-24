b0615eba2cd727c872245dca91de9bee56490403
LKH <105769152+CharmStrange@users.noreply.github.com>
2025-08-24 14:03:30 +0900

    Update script.r

--- script.r
+++ script.r
@@ -1,14 +1,26 @@
 print("Hello, goorm!")
+print("")
 
+# Preparation
 csv <- read.table("ts.csv", header=T, sep=',')
+FilteryWord1 <- "광고노출수 : "
+FilteryWord2 <- "광고클릭수 : "
+FilteryWord3 <- "예상 수익 : "
+#
 
 expedition <- function(CSV) {
-	MonthlyPosts <- as.vector( CSV[1:32, 1:1] ) # 일단 행렬인데 열 벡터
-	MostPosts <- as.vector( CSV[2:6, 6:36] ) # 얘네를 어떡하지
-	MonthlyParameters <- as.vector( CSV[8:11, 6:36] ) # 얘네를 어떡하지
-	MonthlyIncome <- as.vector( CSV[11:11, 6:36] ) # 행렬인데 행 벡터
+	MonthlyPosts <- CSV[2:9, 1:1] # 일단 행렬인데 열 벡터 / 왜 NULL ?
+	MostPosts <- CSV[2:6, 6:36] # 얘네를 어떡하지 / 5행 31열
+	MonthlyParameters <- CSV[8:11, 6:36] # 얘네를 어떡하지 / 4행 31열
 	
-	print("벡터 인덱싱을 통해 정보 확인 : 모든 게시물, 최다 조회 게시물, 게시물 속성 정보, 종합 수익")
+	MonthlyIncome <- as.vector(MonthlyParameters[3:3, 1:31])
+	
+	for (cell in MonthlyIncome) {gsub(FilteryWord3, "", cell)}
+	
+	print(MonthlyIncome)
+	
+	print("인덱싱을 통해 정보 확인 : 모든 게시물, 최다 조회 게시물, 게시물 속성 정보, 종합 수익")
 }
 
 expedition(csv)
+
